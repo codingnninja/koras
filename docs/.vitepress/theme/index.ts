308a2e11e3c2a98b5d3586aea374dd55044c58f9
ayobami_ogundiran <ayobami_ogundiran@yahoo.com>
2024-11-02 06:23:28 +0100

    delete unneeded files from assets

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,25 +1,3 @@
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
-import {codeToHtml} from "shiki";
-import { transformerRenderWhitespace } from '@shikijs/transformers';
-import { $render } from '@codingnninja/render'
 export default DefaultTheme;
-
-let code = await codeToHtml('<div id="student"></div>', {
-    lang: 'html',
-    theme: 'github-dark',
-    transformers: [transformerRenderWhitespace()]
-  });
-
-  const Counter = (count = 0) => {
-    console.log('get here')
-    return `
-      <div id="counter-1">
-        <button 
-          onClick="$render(Counter, ${count + 1})" 
-          style="height:30px; width:100px">Count is ${count}
-        </button>
-      </div>
-    `;
-  };
-
--- docs/public/test.js
+++
@@ -1,2 +0,0 @@
-// import * as shiki from '../node_modules/@shikijs';
-// console.log(shiki);
