adac8720673b0ce8dff539b2667ed866018dc9bb
ayobami_ogundiran <ayobami_ogundiran@yahoo.com>
2026-04-10 19:55:41 +0100

    refactor: add index.js in source to use all utils at once

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// export * from "./render/index.js";
+// export * from "./query/index.js";
+export * from "./render/index.js";
+export * from "./query/index.js";
+
+export { $register } from "./render/componentRegistration.js";
