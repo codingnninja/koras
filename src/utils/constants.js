3abaa1ce9bfccafce0c866b405bd49533cd4f427
ayobami_ogundiran <ayobami_ogundiran@yahoo.com>
2026-04-10 19:54:34 +0100

    refactor: add utils and its units

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+const renderIdentity = "_9s35Ufa7M67wghwT_";
+
+const STRING = "string";
+const NUMBER = "number";
+const FUNCTION = "function";
+const BOOLEAN = "boolean";
+const SYMBOL = "symbol";
+const BIG_INT = "bigint";
+
+const ZERO = 0;
+const ONE = 1;
+
+const UNDEFINED = undefined || "undefined";
+
+globalThis.ks = globalThis.ks || {};
